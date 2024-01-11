60367221b8dc5dd4757691505af8e07bbe8070e9
Thomas <thomashess1012@gmail.com>
2024-01-10 23:05:32 -0300

    minor fix

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -7,8 +7,7 @@ export default function HeroSection() {
         <div
           className="hero h-96 md:h-[500px] rounded-box overflow-hidden"
           style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+            backgroundImage: "url(src/image/1.jpeg)",
           }}
         >
           <div className="hero-overlay bg-opacity-60 bg-ghost"></div>
